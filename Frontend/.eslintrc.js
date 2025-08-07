5301cc9e80853b004bf418b902a8ff9671f150f4
Atyos35 <valerian.guemene@gmail.com>
2025-08-07 15:56:41 +0200

    INSTALL : NUXT3 + Vue3 + Quasar

--- Frontend/.eslintrc.js
+++ Frontend/.eslintrc.js
@@ -1,12 +1,52 @@
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2022: true
+  },
   extends: [
-    '@nuxt/eslint-config'
+    '@nuxt/eslint-config',
+    '@vue/eslint-config-typescript',
+    'plugin:vue/vue3-recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'vue'
   ],
   rules: {
-    // Règles personnalisées si nécessaire
+    // Règles TypeScript
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    
+    // Règles Vue
     'vue/multi-word-component-names': 'off',
-    'vue/no-unused-vars': 'warn',
-    '@typescript-eslint/no-unused-vars': 'warn'
-  }
-} 
+    'vue/no-v-html': 'warn',
+    'vue/require-default-prop': 'off',
+    'vue/require-prop-types': 'off',
+    
+    // Règles générales
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-template': 'error'
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off'
+      }
+    }
+  ]
+}
