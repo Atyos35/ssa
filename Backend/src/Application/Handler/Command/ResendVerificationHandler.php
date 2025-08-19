156f05506a8fa61275139ba8a25805f1f3e39c72
Atyos35 <valerian.guemene@gmail.com>
2025-08-19 12:38:53 +0200

    FIX : tests fonctionnels

--- Backend/src/Application/Handler/Command/ResendVerificationHandler.php
+++ Backend/src/Application/Handler/Command/ResendVerificationHandler.php
@@ -5,14 +5,16 @@ namespace App\Application\Handler\Command;
 use App\Application\Command\CommandInterface;
 use App\Application\Command\ResendVerificationCommand;
 use App\Application\Handler\CommandHandlerInterface;
+use App\Domain\Entity\User;
 use App\Domain\Service\EmailVerificationService;
 use Doctrine\ORM\EntityManagerInterface;
-// Supprimé: use Symfony\Component\Uid\Uuid;
+use Symfony\Component\Uid\Uuid;
 
 class ResendVerificationHandler implements CommandHandlerInterface
 {
     public function __construct(
-        private readonly EntityManagerInterface $entityManager
+        private readonly EntityManagerInterface $entityManager,
+        private readonly EmailVerificationService $emailVerificationService
     ) {}
 
     public function handle(CommandInterface $command): void
@@ -34,15 +36,14 @@ class ResendVerificationHandler implements CommandHandlerInterface
         }
 
         // Générer un nouveau token de vérification
-        // Supprimé: $token = Uuid::v4()->toRfc4122();
-        $token = bin2hex(random_bytes(32)); // Alternative plus simple
+        $token = Uuid::v4()->toRfc4122();
         $user->setEmailVerificationToken($token);
         $user->setEmailVerificationExpiresAt(new \DateTimeImmutable('+24 hours'));
         
         $this->entityManager->persist($user);
         $this->entityManager->flush();
 
-        // TODO: Envoyer l'email (injecter le service EmailVerificationService)
-        // $this->emailVerificationService->sendVerificationEmail($user, $token);
+        // Envoyer l'email de vérification
+        $this->emailVerificationService->sendVerificationEmail($user, $token);
     }
 } 
